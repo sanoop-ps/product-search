afe0c2294fed8bdad46511440bc330b6588d4781
CWSPS154 <codewithsps154@gmail.com>
2023-08-06 23:58:36 +0530

    config updated

--- VisualSearch/Model/System/Config.php
+++ VisualSearch/Model/System/Config.php
@@ -9,47 +9,55 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 
 class Config
 {
+    public const VISUAL_SEARCH_API_ENABLED = 'visual_search/vs_general/enabled';
+    public const VISUAL_SEARCH_API_PROJECT_NAME = 'visual_search/vs_general/project_name';
+    public const VISUAL_SEARCH_API_LOCATION = 'visual_search/vs_general/location';
+    public const VISUAL_SEARCH_API_PRODUCT_SET = 'visual_search/vs_general/product_set';
+    public const GOOGLE_STORAGE_API_BUCKET_NAME = 'visual_search/google_storage/bucket_name';
+    public const GOOGLE_STORAGE_API_KEY = 'visual_search/google_storage/gs_api_key';
     /**
      * API URL config path
      *
      * @var string VISUAL_SEARCH_API_BASE_URL
      */
-    public const VISUAL_SEARCH_API_BASE_URL = 'visual_search/gv_api_management/vs_endpoint/';
+    public const VISUAL_SEARCH_API_BASE_URL = 'visual_search/vs_api/vs_endpoint';
     /**
      * API client id config path
      *
      * @var string VISUAL_SEARCH_API_CLIENT_ID
      */
-    public const VISUAL_SEARCH_API_CLIENT_ID = 'visual_search/gv_api_management/vs_client_id';
+    public const VISUAL_SEARCH_API_CLIENT_ID = 'visual_search/vs_api/vs_client_id';
     /**
      * API secret key config path
      *
      * @var string VISUAL_SEARCH_API_CLIENT_SECRET
      */
-    public const VISUAL_SEARCH_API_CLIENT_SECRET = 'visual_search/gv_api_management/vs_client_secret';
+    public const VISUAL_SEARCH_API_CLIENT_SECRET = 'visual_search/vs_api/vs_client_secret';
     /**
      * API callback url config path
      *
      * @var string VISUAL_SEARCH_CALLBACK_URL
      */
-    public const VISUAL_SEARCH_CALLBACK_URL = 'visual_search/gv_api_management/vs_callback_url';
+    public const VISUAL_SEARCH_CALLBACK_URL = 'visual_search/vs_api/vs_callback_url';
     /**
      * API auth url config path
      *
      * @var string VISUAL_SEARCH_AUTH_URL
      */
-    public const VISUAL_SEARCH_AUTH_URL = 'visual_search/gv_api_management/vs_auth_url';
+    public const VISUAL_SEARCH_AUTH_URL = 'visual_search/vs_api/vs_auth_url';
     /**
      * API access token url config path
      *
      * @var string VISUAL_SEARCH_ACCESS_TOKEN_URL
      */
-    public const VISUAL_SEARCH_ACCESS_TOKEN_URL = 'visual_search/gv_api_management/vs_access_token_url';/**
+    public const VISUAL_SEARCH_ACCESS_TOKEN_URL = 'visual_search/vs_api/vs_scope';
+    /**
      * API scope config path
      *
      * @var string VISUAL_SEARCH_SCOPE
      */
-    public const VISUAL_SEARCH_SCOPE = 'visual_search/gv_api_management/vs_scope';
+    public const VISUAL_SEARCH_SCOPE = 'visual_search/vs_api/vs_scope';
+    public const VISUAL_SEARCH_BEARER_TOKEN = 'visual_search/vs_api/vs_token';
 
     /**
      * @param ScopeConfigInterface $scopeConfig
@@ -57,6 +65,36 @@ class Config
     public function __construct(
         private readonly ScopeConfigInterface   $scopeConfig
     ) {
+    }
+
+    public function getVisualSearchEnabled(): bool
+    {
+        return $this->scopeConfig->isSetFlag(self::VISUAL_SEARCH_API_ENABLED);
+    }
+
+    public function getVisualSearchProjectName(): string|null
+    {
+        return $this->scopeConfig->getValue(self::VISUAL_SEARCH_API_PROJECT_NAME);
+    }
+
+    public function getVisualSearchLocation(): string|null
+    {
+        return $this->scopeConfig->getValue(self::VISUAL_SEARCH_API_LOCATION);
+    }
+
+    public function getVisualSearchProductSet(): string|null
+    {
+        return $this->scopeConfig->getValue(self::VISUAL_SEARCH_API_PRODUCT_SET);
+    }
+
+    public function getGoogleStorageBucketName(): string|null
+    {
+        return $this->scopeConfig->getValue(self::GOOGLE_STORAGE_API_BUCKET_NAME);
+    }
+
+    public function getGoogleStorageApiKey(): string|null
+    {
+        return $this->scopeConfig->getValue(self::GOOGLE_STORAGE_API_KEY);
     }
 
     /**
@@ -129,9 +167,13 @@ class Config
      * @return string
      * @throws Exception
      */
-    public function getVisualScope(): string|null
+    public function getVisualSearchScope(): string|null
     {
         return $this->scopeConfig->getValue(self::VISUAL_SEARCH_SCOPE);
     }
 
+    public function getVisualSearchApiToken(): string|null
+    {
+        return $this->scopeConfig->getValue(self::VISUAL_SEARCH_BEARER_TOKEN);
+    }
 }
